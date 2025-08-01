6da51b3aa54a9527ed73fa61013d08636086bbec
izzyjhuang <izzyjhuang@gmail.com>
2025-08-01 11:32:55 -0500

    removed #

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
